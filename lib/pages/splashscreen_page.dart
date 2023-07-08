b38cc7fc1edd36bafa48c22c7f33bf027a43845a
HanifanHI <hanifanisnanto@gmail.com>
2023-07-08 22:40:01 +0700

    update splash

--- lib/pages/splashscreen_page.dart
+++ lib/pages/splashscreen_page.dart
@@ -19,7 +19,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   Widget build(BuildContext context) {
     Timer(
-      const Duration(seconds: 5),
+      const Duration(seconds: 3),
       () {
         Navigator.pushAndRemoveUntil(
           context,
